79db6be77e4edec7de467575be67666aeab9bc35
Sant268 <kunalbooch@hotmail.com>
2021-03-14 11:28:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const STREAM_CALENDAR_CONFIG = {
   "documentTitle": "Stream Calendar",
   "alertHTML": "",
   "footerHTML": "Made with ♥ by <a href=\"https://ko-fi.com/sant268\">Sant268</a>",
+  //footer fulfills the attribution, but can be removed. 
   "daysToShow": 7,
   "useAlternateButtonStyle": false,
   "enableGoogleTranslate": true,
